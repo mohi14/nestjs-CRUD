c941fe636063a35ab32b9f0a705925455585dea2
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-22 13:06:22 +0600

    nestjs crud opreration done

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Put, Res, Param, HttpStatus, Delete,Get,Body} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDto } from 'src/dto/create-student.dto';
+import { UpdateStuendtDto } from 'src/dto/update-student.dto';
 
 
 @Controller('student')
@@ -26,5 +27,54 @@ try {
     }
 
     @Get()
-    async g
+    async getStudents(@Res()response){
+        try {
+            const studentData = await this.studentService.getAllStudents()
+            return response.status(HttpStatus.OK).json({
+                message: "All students data found successfully",
+                studentData
+            })
+        } catch (err) {
+            return response.status(err.status).json(err.response)
+        }
+    }
+
+    @Put('/:id')
+    async updateStudent(@Res() response, @Param('id') studentId:string, @Body() updateStudentDto:UpdateStuendtDto){
+        try {
+            const existingStudent = await this.studentService.updateStudent(studentId, updateStudentDto)
+            return response.status(HttpStatus.OK).json({
+                message: "Student has been successfully updated",
+                existingStudent,
+            })
+        } catch (err) {
+            return response.status(err.status).json(err.response);
+        }
+    }
+
+    @Delete('/:id')
+    async deleteStudent(@Res() response, @Param('id') studentId:string){
+try {
+    const deletedStudent = await this.studentService.deleteStudent(studentId)
+    return response.status(HttpStatus.OK).json({
+        message: "Student deleted successfully",
+        deletedStudent
+    })
+} catch (err) {
+    return response.status(err.status).json(err.response);
+}
+    }
+
+    @Get('/:id')
+    async getStudent(@Res() response, @Param('id') studentId: string){
+        try {
+            const existingStudent = await this.studentService.getStudent(studentId)
+            return response.status(HttpStatus.OK).json({
+                message: "Student found successfully",
+                existingStudent
+            })
+        } catch (err) {
+            return response.status(err.status).json(err.response);
+        }
+    }
 }
